884b91f198070033f9ad54ab1a2fc6f87462e3ca
Dmitry P <dmitry@pereslegin.ru>
2019-01-01 19:09:07 +0300

    Add missing dependency

--- src/app/picture-moder-vote/picture-moder-vote.module.ts
+++ src/app/picture-moder-vote/picture-moder-vote.module.ts
@@ -5,10 +5,11 @@ import { PictureModerVoteModalComponent } from './picture-moder-vote/modal/modal
 import { TranslateModule } from '@ngx-translate/core';
 import { UserModule } from '../user/user.module';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [PictureModerVoteComponent, PictureModerVoteModalComponent],
-  imports: [CommonModule, TranslateModule, UserModule, FormsModule],
+  imports: [CommonModule, TranslateModule, UserModule, FormsModule, NgbModule],
   entryComponents: [PictureModerVoteModalComponent],
   exports: [PictureModerVoteComponent]
 })
