9cc6c06c846bd094e4d2f82c1d9b30088e7a536d
Dmitry P <dmitry@pereslegin.ru>
2019-01-07 16:42:15 +0300

    Add missed TranslateModule

--- src/app/info/info.module.ts
+++ src/app/info/info.module.ts
@@ -6,9 +6,10 @@ import { InfoSpecRowComponent } from './spec/row/row.component';
 import { InfoSpecComponent } from './spec/spec.component';
 import { InfoTextComponent } from './text/text.component';
 import { UserModule } from '../user/user.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [InfoSpecRowComponent, InfoSpecComponent, InfoTextComponent],
-  imports: [CommonModule, InfoRoutingModule, UserModule]
+  imports: [CommonModule, InfoRoutingModule, UserModule, TranslateModule]
 })
 export class InfoModule {}
