f565df89bad05f03ebd52caf41058d42c2f60a38
Dmitry P <dmitry@pereslegin.ru>
2019-03-26 23:21:20 +0300

    Change sounar project id

--- build-dev.sh
+++ build-dev.sh
@@ -1,1 +1,1 @@
-./node_modules/.bin/ng build --watch --output-path=../autowp/frontend/ng/ --base-href=/ng/ --extra-webpack-config webpack.extra.js
+./node_modules/.bin/ng build --output-path=../autowp/frontend/ng/ --base-href=/ng/ --extra-webpack-config webpack.extra.js --aot --vendor-chunk --build-optimizer=false --source-map=false
