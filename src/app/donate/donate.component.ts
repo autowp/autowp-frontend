b70e8587a5ff61b38c4c7093f39bb6d192357f80
Dmitry P <dmitry@pereslegin.ru>
2020-04-05 19:59:12 +0300

    Fix donate form

--- src/app/donate/donate.component.ts
+++ src/app/donate/donate.component.ts
@@ -2,6 +2,7 @@ import { Component, Injectable } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { PageEnvService } from '../services/page-env.service';
 import { LanguageService } from '../services/language';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-donate',
@@ -9,13 +10,14 @@ import { LanguageService } from '../services/language';
 })
 @Injectable()
 export class DonateComponent {
-  public frameUrl: string;
+  public frameUrl: SafeResourceUrl;
   public language: string;
 
   constructor(
     private translate: TranslateService,
     private pageEnv: PageEnvService,
-    private languageService: LanguageService
+    private languageService: LanguageService,
+    private domSanitizer: DomSanitizer
   ) {
     this.language = this.languageService.getLanguage();
 
@@ -34,7 +36,7 @@ export class DonateComponent {
     this.translate
       .get(['donate/target', 'donate/project', 'donate/comment-hint'])
       .subscribe((translations: string[]) => {
-        const map = {
+        /*const map = {
           account: '41001161017513',
           quickpay: 'donate',
           'payment-type-choice': 'on',
@@ -48,16 +50,36 @@ export class DonateComponent {
           comment: 'on',
           hint: translations[2],
           successURL: 'https://' + window.location.host + '/donate/success'
+        };*/
+
+        console.log(translations);
+
+        const map = {
+          writer: 'seller',
+          targets: translations['donate/target'],
+          'targets-hint': translations['donate/comment-hint'],
+          'default-sum': '100',
+          'payment-type-choice': 'on',
+          'mobile-payment-type-choice': 'on',
+          successURL: 'https://' + window.location.host + '/donate/success',
+          comment: 'on',
+          hint: translations['donate/comment-hint'],
+          account: '41001161017513',
+          quickpay: 'shop',
+          'button-text': '14',
+          'target-visibility': 'on',
+          'project-name': translations['donate/project'],
+          'project-site': 'https://' + window.location.host + '/',
         };
 
-        const url = new URL('https://money.yandex.ru/embed/donate.xml');
+        const url = new URL('https://money.yandex.ru/quickpay/shop-widget');
         for (const key in map) {
           if (map.hasOwnProperty(key)) {
             url.searchParams.append(key, map[key]);
           }
         }
 
-        this.frameUrl = url.toString();
+        this.frameUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(url.toString());
       });
   }
 }
