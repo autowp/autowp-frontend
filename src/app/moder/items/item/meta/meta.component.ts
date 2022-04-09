570e260304a6db3b8d824d2b97ac0bcd614050c1
Dmitry P <dmitry@pereslegin.ru>
2022-04-09 10:09:28 +0300

    moder/item/meta component using BehaviorSubject on Input

--- src/app/moder/items/item/meta/meta.component.ts
+++ src/app/moder/items/item/meta/meta.component.ts
@@ -1,22 +1,39 @@
-import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {APIItem, ItemService} from '../../../../services/item';
 import {ACLService, Privilege, Resource} from '../../../../services/acl.service';
 import {APIItemVehicleTypeGetResponse, APIService} from '../../../../services/api.service';
-import {EMPTY, forkJoin} from 'rxjs';
-import {catchError, tap} from 'rxjs/operators';
+import {BehaviorSubject, EMPTY, forkJoin, Observable, of} from 'rxjs';
+import {catchError, map, switchMap, tap} from 'rxjs/operators';
+import {ItemType} from '../../../../../../generated/spec.pb';
 
 @Component({
   selector: 'app-moder-items-item-meta',
   templateUrl: './meta.component.html'
 })
-export class ModerItemsItemMetaComponent implements OnChanges {
-  @Input() item: APIItem;
+export class ModerItemsItemMetaComponent {
+  @Input() set item(item: APIItem) { this.item$.next(item); };
+  public item$ = new BehaviorSubject<APIItem>(null);
 
-  public loading = 0;
+  public loadingNumber = 0;
 
   public canEditMeta$ = this.acl.isAllowed(Resource.CAR, Privilege.EDIT_META);
-  public vehicleTypeIDs: number[] = [];
   public invalidParams: any;
+
+  public vehicleTypeIDs$: Observable<number[]> = this.item$.pipe(
+    switchMap(item => {
+      if (item.item_type_id === ItemType.ITEM_TYPE_VEHICLE || item.item_type_id === ItemType.ITEM_TYPE_TWINS) {
+        return this.api.request<APIItemVehicleTypeGetResponse>('GET', 'item-vehicle-type', {
+          params: {
+            item_id: item.id.toString()
+          }
+        }).pipe(
+          map(response => response.items.map(row => row.vehicle_type_id)),
+        );
+      }
+
+      return of([])
+    })
+  );
 
   constructor(
     private acl: ACLService,
@@ -24,66 +41,46 @@ export class ModerItemsItemMetaComponent implements OnChanges {
     private itemService: ItemService
   ) {}
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes.item) {
-      if (this.item.item_type_id === 1 || this.item.item_type_id === 4) {
-        this.loading++;
-        this.api
-          .request<APIItemVehicleTypeGetResponse>('GET', 'item-vehicle-type', {
-            params: {
-              item_id: this.item.id.toString()
-            }
-          })
-          .subscribe(
-            response => {
-              this.vehicleTypeIDs = response.items.map(row => row.vehicle_type_id);
-
-              this.loading--;
-            },
-            () => {
-              this.loading--;
-            }
-          );
-      }
-    }
-  }
-
-  public saveMeta() {
-    this.loading++;
+  public saveMeta(item: APIItem, vehicleTypeIDs: number[]) {
+    this.loadingNumber++;
 
     const data = {
       // item_type_id: this.$state.params.item_type_id,
-      name: this.item.name,
-      full_name: this.item.full_name,
-      catname: this.item.catname,
-      body: this.item.body,
-      spec_id: this.item.spec_id,
-      begin_model_year: this.item.begin_model_year,
-      end_model_year: this.item.end_model_year,
-      begin_model_year_fraction: this.item.begin_model_year_fraction,
-      end_model_year_fraction: this.item.end_model_year_fraction,
-      begin_year: this.item.begin_year,
-      begin_month: this.item.begin_month,
-      end_year: this.item.end_year,
-      end_month: this.item.end_month,
-      today: this.item.today,
-      produced: this.item.produced,
-      produced_exactly: this.item.produced_exactly,
-      is_concept: this.item.is_concept,
-      is_group: this.item.is_group,
-      lat: this.item.lat,
-      lng: this.item.lng
+      name: item.name,
+      full_name: item.full_name,
+      catname: item.catname,
+      body: item.body,
+      spec_id: item.spec_id,
+      begin_model_year: item.begin_model_year,
+      end_model_year: item.end_model_year,
+      begin_model_year_fraction: item.begin_model_year_fraction,
+      end_model_year_fraction: item.end_model_year_fraction,
+      begin_year: item.begin_year,
+      begin_month: item.begin_month,
+      end_year: item.end_year,
+      end_month: item.end_month,
+      today: item.today,
+      produced: item.produced,
+      produced_exactly: item.produced_exactly,
+      is_concept: item.is_concept,
+      is_group: item.is_group,
+      lat: item.lat,
+      lng: item.lng
     };
 
     forkJoin([
-      this.api.request<void>('PUT', 'item/' + this.item.id, {body: data}).pipe(
+      this.api.request<void>('PUT', 'item/' + item.id, {body: data}).pipe(
         catchError(response => {
           this.invalidParams = response.error.invalid_params;
           return EMPTY;
         }),
         tap(() => (this.invalidParams = {}))
       ),
-      this.itemService.setItemVehicleTypes(this.item.id, this.vehicleTypeIDs)
-    ]).subscribe(() => {}, () => {}, () => this.loading--);
+      this.itemService.setItemVehicleTypes(item.id, vehicleTypeIDs)
+    ]).subscribe({
+      complete: () => {
+        this.loadingNumber--
+      }
+    });
   }
 }
