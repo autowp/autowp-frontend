fea0834221e210290cb440e8d0003162a5abda22
Dmitry P <dmitry@pereslegin.ru>
2022-05-07 05:20:22 +0300

    Migrate to async

--- src/app/categories/category-item.component.ts
+++ src/app/categories/category-item.component.ts
@@ -1,13 +1,12 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {APIItem, ItemService} from '../services/item';
-import {Observable, of, Subscription} from 'rxjs';
+import {combineLatest, Observable, of} from 'rxjs';
 import {PageEnvService} from '../services/page-env.service';
 import {ActivatedRoute} from '@angular/router';
-import {map, switchMap, tap} from 'rxjs/operators';
+import {debounceTime, distinctUntilChanged, map, shareReplay, switchMap, tap} from 'rxjs/operators';
 import {ACLService, Privilege, Resource} from '../services/acl.service';
-import {APIPaginator} from '../services/api.service';
-import {APIItemParent, APIItemParentGetResponse, ItemParentService} from '../services/item-parent';
-import {APIPicture, APIPictureGetResponse, PictureService} from '../services/picture';
+import {ItemParentService} from '../services/item-parent';
+import {APIPicture, PictureService} from '../services/picture';
 import {CatagoriesService} from './service';
 import { getItemTypeTranslation } from '../utils/translations';
 import {ItemType} from '../../../generated/spec.pb';
@@ -25,144 +24,51 @@ interface PictureRoute {
   route: string[];
 }
 
-interface ItemParentRoute {
-  item: APIItemParent;
-  parentRouterLink: string[];
-}
-
-function currentRouterLink(category: APIItem, current: APIItem, pathCatnames: string[]): string[] {
-  if (current.item_type_id === ItemType.ITEM_TYPE_CATEGORY) {
-    return ['/category', current.catname];
-  }
-
-  return ['/category', category.catname].concat(pathCatnames);
-}
-
-function getCurrentRouterLinkPrefix(category: APIItem, current: APIItem, pathCatnames: string[]): string[] {
-  if (! category) {
-    return null;
-  }
-
-  if (current.item_type_id === ItemType.ITEM_TYPE_CATEGORY) {
-    return ['/category', current.catname];
-  }
-
-  return [
-    '/category',
-    category.catname
-  ].concat(pathCatnames);
-}
-
-function itemRouterLink(category: APIItem, pathCatnames: string[], itemParent: APIItemParent): string[] {
-  if (itemParent.item.item_type_id === ItemType.ITEM_TYPE_CATEGORY) {
-    return ['/category', itemParent.item.catname];
-  }
-
-  return ['/category', category.catname].concat(pathCatnames, [
-    itemParent.catname
-  ]);
-}
-
 @Component({
   selector: 'app-categories-category-item',
   templateUrl: './category-item.component.html'
 })
-export class CategoriesCategoryItemComponent implements OnInit, OnDestroy {
-  private sub: Subscription;
-  public current: APIItem;
-  public category: APIItem;
-  public items: ItemParentRoute[] = [];
+export class CategoriesCategoryItemComponent {
   public isModer$ = this.acl.isAllowed(Resource.GLOBAL, Privilege.MODERATE);
   public canAddCar$ = this.acl.isAllowed(Resource.CAR, Privilege.ADD);
-  public paginator: APIPaginator;
-  public path: PathItem[];
-  public pictures: PictureRoute[];
-  public item: APIItem;
-  public currentRouterLinkPrefix: string[];
 
-  constructor(
-    private itemService: ItemService,
-    private itemParentService: ItemParentService,
-    private pictureService: PictureService,
-    private pageEnv: PageEnvService,
-    private route: ActivatedRoute,
-    private acl: ACLService,
-    private categoriesService: CatagoriesService
-  ) {}
+  private categoryData$ = this.categoriesService.categoryPipe(this.route).pipe(
+    tap(({current}) => {
+      this.pageEnv.set({
+        layout: {
+          needRight: false
+        },
+        nameTranslated: current.name_text,
+        pageId: 22
+      });
+    }),
+    shareReplay(1)
+  );
 
-  ngOnInit(): void {
-    this.sub = this.categoriesService.categoryPipe(this.route)
-      .pipe(
-        tap(data => {
-          this.current = data.current;
-          this.category = data.category;
-          this.path = data.pathItems;
-          this.pageEnv.set({
-            layout: {
-              needRight: false
-            },
-            nameTranslated: data.current.name_text,
-            pageId: 22
-          });
-        }),
-        switchMap(data => this.route.queryParamMap.pipe(
-          map(query => ({
-            category: data.category,
-            current: data.current,
-            pathCatnames: data.pathCatnames,
-            page: parseInt(query.get('page'), 10)
-          }))
-        )),
-        switchMap(
-          data => this.getItemParents(data.category, data.current, data.pathCatnames, data.page).pipe(
-              map(response => ({
-                items: response.items,
-                category: data.category,
-                current: data.current,
-                pathCatnames: data.pathCatnames
-              }))
-            )
-        ),
-        switchMap(data => {
-          this.pictures = null;
-          this.item = null;
+  public current$: Observable<APIItem> = this.categoryData$.pipe(
+    map(({current}) => current),
+    shareReplay(1)
+  );
 
-          if (data.current.item_type_id === ItemType.ITEM_TYPE_CATEGORY) {
-            return of(null);
-          }
+  public category$: Observable<APIItem> = this.categoryData$.pipe(
+    map(({category}) => category),
+    shareReplay(1)
+  );
 
-          if (data.items.length > 0) {
-            return this.getPictures(data.category, data.current, data.pathCatnames);
-          }
+  public path$: Observable<PathItem[]> = this.categoryData$.pipe(
+    map(({pathItems}) => pathItems),
+    shareReplay(1)
+  );
 
-          return this.getItems(data.category, data.current, data.pathCatnames);
-        })
-      )
-      .subscribe();
-  }
+  private page$ = this.route.queryParamMap.pipe(
+    map(query => parseInt(query.get('page'), 10)),
+    distinctUntilChanged(),
+    debounceTime(10)
+  );
 
-  private getItems(category: APIItem, current: APIItem, pathCatnames: string[]): Observable<APIItem> {
-    return this.itemService
-      .getItem(current.id, {
-        fields: [
-          'catname,name_html,name_default,description,has_text,produced,accepted_pictures_count',
-          'design,engine_vehicles',
-          'can_edit_specs,specs_route',
-          'twins_groups',
-          'childs_count,total_pictures,preview_pictures.picture.name_text'
-        ].join(',')
-      })
-      .pipe(
-        tap(item => {
-          this.item = item;
-          this.currentRouterLinkPrefix = getCurrentRouterLinkPrefix(category, current, pathCatnames);
-        })
-      );
-  }
-
-  private getItemParents(category: APIItem, current: APIItem, pathCatnames: string[], page: number): Observable<APIItemParentGetResponse> {
-    return this.itemParentService
-      .getItems({
+  public itemParents$ = combineLatest([this.categoryData$, this.page$]).pipe(
+    switchMap(([{category, current, pathCatnames}, page]) => {
+      return this.itemParentService.getItems({
         fields: [
           'item.name_html,item.name_default,item.description,item.has_text,item.produced,item.accepted_pictures_count',
           'item.design,item.engine_vehicles',
@@ -174,42 +80,89 @@ export class CategoriesCategoryItemComponent implements OnInit, OnDestroy {
         page,
         parent_id: current.id,
         order: 'categories_first'
-      })
-      .pipe(
-        tap(response => {
-          this.items = response.items.map(itemParent => ({
+      }).pipe(
+        map(response => ({
+          items: response.items.map(itemParent => ({
             item: itemParent,
-            parentRouterLink: itemRouterLink(category, pathCatnames, itemParent)
-          }));
-          this.paginator = response.paginator;
-        })
+            parentRouterLink: [
+              '/category',
+              ...(
+                itemParent.item.item_type_id === ItemType.ITEM_TYPE_CATEGORY
+                  ? [itemParent.item.catname]
+                  : [category.catname, ...pathCatnames, itemParent.catname]
+              )
+            ]
+          })),
+          paginator: response.paginator
+        }))
       );
-  }
+    }),
+    shareReplay(1)
+  );
 
-  private getPictures(category: APIItem, current: APIItem, pathCatnames: string[]): Observable<APIPictureGetResponse> {
-    return this.pictureService
-      .getPictures({
+  public pictures$: Observable<PictureRoute[]> = combineLatest([this.categoryData$, this.itemParents$]).pipe(
+    switchMap(([{current, category, pathCatnames}, itemParents]) => {
+      if (current.item_type_id === ItemType.ITEM_TYPE_CATEGORY || itemParents.items.length <= 0) {
+        return of(null);
+      }
+
+      return this.pictureService.getPictures({
         exact_item_id: current.id,
         limit: 4,
         fields: 'thumb_medium,name_text'
-      })
-      .pipe(
-        tap(response => {
-          this.pictures = response.pictures.map(pic => ({
-            picture: pic,
-            route: currentRouterLink(category, current, pathCatnames).concat(['pictures', pic.identity])
-          }));
-        })
+      }).pipe(
+        map(response => response.pictures.map(pic => ({
+          picture: pic,
+          route: ['/category', category.catname, ...(current.item_type_id === ItemType.ITEM_TYPE_CATEGORY ? [] : pathCatnames), 'pictures', pic.identity]
+        })))
       );
-  }
+    })
+  );
 
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
-  }
+  public currentRouterLinkPrefix$ = this.categoryData$.pipe(
+    map(({current, category, pathCatnames}) => {
+      if (! category) {
+        return null;
+      }
+
+      if (current.item_type_id === ItemType.ITEM_TYPE_CATEGORY) {
+        return ['/category', current.catname];
+      }
+
+      return ['/category', category.catname, ...(current.item_type_id === ItemType.ITEM_TYPE_CATEGORY ? [] : pathCatnames)];
+    })
+  );
+
+  public item$: Observable<APIItem> = combineLatest([this.categoryData$, this.itemParents$]).pipe(
+    switchMap(([{current}, itemParents]) => {
+      if (current.item_type_id === ItemType.ITEM_TYPE_CATEGORY || itemParents.items.length > 0) {
+        return of(null);
+      }
+
+      return this.itemService.getItem(current.id, {
+        fields: [
+          'catname,name_html,name_default,description,has_text,produced,accepted_pictures_count',
+          'design,engine_vehicles',
+          'can_edit_specs,specs_route',
+          'twins_groups',
+          'childs_count,total_pictures,preview_pictures.picture.name_text'
+        ].join(',')
+      });
+    })
+  )
+
+  constructor(
+    private itemService: ItemService,
+    private itemParentService: ItemParentService,
+    private pictureService: PictureService,
+    private pageEnv: PageEnvService,
+    private route: ActivatedRoute,
+    private acl: ACLService,
+    private categoriesService: CatagoriesService
+  ) {}
 
   public dropdownOpenChange(item: PathItem) {
     if (!item.loaded) {
-
       this.itemService.getItems({
         fields: 'catname,name_html',
         parent_id: item.parent_id,
