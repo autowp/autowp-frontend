ae4bb26c0bc4bdff15e239d5f9aa10fe6aca4f26
Dmitry P <dmitry@pereslegin.ru>
2022-12-03 00:59:49 +0300

    feat: bump to ng v15

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,16 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import * as Sentry from "@sentry/angular";
-import {BrowserTracing} from '@sentry/tracing';
+// import * as Sentry from "@sentry/angular";
+// import {BrowserTracing} from '@sentry/tracing';
 
 if (environment.production) {
   enableProdMode();
 }
 
-const version = require('./version.json')
+// const version = require('./version.json')
 
-Sentry.init({
+/*Sentry.init({
   dsn: environment.sentry.dsn,
   environment: environment.sentry.environment,
   release: version.release,
@@ -22,7 +22,7 @@ Sentry.init({
     }),
   ],
   tracesSampleRate: 1.0,
-});
+});*/
 
 platformBrowserDynamic().bootstrapModule(AppModule, { preserveWhitespaces: true })
   .catch(err => console.log(err));
