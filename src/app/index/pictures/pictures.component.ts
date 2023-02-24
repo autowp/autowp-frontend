26ea93873c222d9549382f532c003ebe1aa94ba4
Dmitry P <dmitry@pereslegin.ru>
2023-02-24 20:14:19 +0300

    feat: index new picture placeholders

--- src/app/index/pictures/pictures.component.ts
+++ src/app/index/pictures/pictures.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {PictureService} from '../../services/picture';
-import {map} from 'rxjs/operators';
+import {map, delay} from 'rxjs/operators';
 
 @Component({
   selector: 'app-index-pictures',
@@ -15,7 +15,10 @@ export class IndexPicturesComponent {
       accepted_in_days: 3,
       fields: 'owner,thumb_medium,votes,views,comments_count,name_html,name_text',
     })
-    .pipe(map((response) => response.pictures));
+    .pipe(
+      delay(5000),
+      map((response) => response.pictures)
+    );
 
   constructor(private pictureService: PictureService) {}
 }
