3963916abe6856fbc75c60fce3f34489f0dea5a3
Dmitry P. <dmitry@pereslegin.ru>
2024-10-21 01:44:51 +0300

    feat: bump currencies

--- src/app/currencies.ts
+++ src/app/currencies.ts
@@ -1,3 +1,3 @@
-export const usdToRub = 92.14;
-export const eurToRub = 100.08;
-export const ethToEur = 3128.72;
+export const usdToRub = 95.88;
+export const eurToRub = 104.2;
+export const ethToEur = 2523.67;
