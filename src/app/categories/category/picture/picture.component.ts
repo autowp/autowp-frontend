0aea25574e486451997204aac2b2ee6adc16d5a6
Dmitry P. <dmitry@pereslegin.ru>
2025-01-25 22:17:08 +0300

    feat: categories/picture via gRPC API

--- src/app/categories/category/picture/picture.component.ts
+++ src/app/categories/category/picture/picture.component.ts
@@ -1,36 +1,49 @@
 import {AsyncPipe} from '@angular/common';
 import {Component, inject} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
-import {CommentsType, ItemType} from '@grpc/spec.pb';
+import {
+  CommentsType,
+  ItemParentCacheListOptions,
+  ItemType,
+  Picture,
+  PictureFields,
+  PictureItemListOptions,
+  PictureItemType,
+  PictureListOptions,
+  PictureModerVoteRequest,
+  PicturesRequest,
+} from '@grpc/spec.pb';
+import {PicturesClient} from '@grpc/spec.pbsc';
+import {LanguageService} from '@services/language';
 import {PageEnvService} from '@services/page-env.service';
-import {APIPicture, PictureService} from '@services/picture';
 import {BehaviorSubject, combineLatest, EMPTY, Observable, of} from 'rxjs';
 import {distinctUntilChanged, map, shareReplay, switchMap, tap} from 'rxjs/operators';
 
 import {CommentsComponent} from '../../../comments/comments/comments.component';
-import {PictureComponent} from '../../../picture/picture.component';
+import {Picture2Component} from '../../../picture/picture2.component';
 import {CategoriesService} from '../../service';
 
 @Component({
-  imports: [PictureComponent, CommentsComponent, AsyncPipe],
+  imports: [CommentsComponent, AsyncPipe, Picture2Component],
   selector: 'app-category-picture',
   templateUrl: './picture.component.html',
 })
 export class CategoryPictureComponent {
-  private readonly route = inject(ActivatedRoute);
-  private readonly pageEnv = inject(PageEnvService);
-  private readonly pictureService = inject(PictureService);
-  private readonly router = inject(Router);
-  private readonly categoriesService = inject(CategoriesService);
+  readonly #route = inject(ActivatedRoute);
+  readonly #pageEnv = inject(PageEnvService);
+  readonly #router = inject(Router);
+  readonly #categoriesService = inject(CategoriesService);
+  readonly #picturesClient = inject(PicturesClient);
+  readonly #languageService = inject(LanguageService);
 
-  private readonly changed$ = new BehaviorSubject<void>(void 0);
+  readonly #changed$ = new BehaviorSubject<void>(void 0);
 
-  private readonly identity$ = this.route.paramMap.pipe(
+  readonly #identity$ = this.#route.paramMap.pipe(
     map((route) => route.get('identity')),
     distinctUntilChanged(),
     switchMap((identity) => {
       if (!identity) {
-        this.router.navigate(['/error-404'], {
+        this.#router.navigate(['/error-404'], {
           skipLocationChange: true,
         });
         return EMPTY;
@@ -39,10 +52,10 @@ export class CategoryPictureComponent {
     }),
   );
 
-  private readonly categoryData$ = this.categoriesService.categoryPipe$(this.route.parent!.parent!).pipe(
+  readonly #categoryData$ = this.#categoriesService.categoryPipe$(this.#route.parent!.parent!).pipe(
     switchMap((data) => {
       if (!data.current) {
-        this.router.navigate(['/error-404'], {
+        this.#router.navigate(['/error-404'], {
           skipLocationChange: true,
         });
         return EMPTY;
@@ -52,7 +65,7 @@ export class CategoryPictureComponent {
     shareReplay({bufferSize: 1, refCount: false}),
   );
 
-  protected readonly currentRouterLinkPrefix$ = this.categoryData$.pipe(
+  protected readonly currentRouterLinkPrefix$ = this.#categoryData$.pipe(
     map(({category, current, pathCatnames}) => {
       if (!category) {
         return null;
@@ -66,35 +79,56 @@ export class CategoryPictureComponent {
     }),
   );
 
-  protected readonly picture$: Observable<APIPicture> = combineLatest([this.categoryData$, this.identity$]).pipe(
-    switchMap(([{current}, identity]) => {
-      const fields =
-        'owner,name_html,name_text,image,preview_large,paginator,subscribed,taken_date,rights,' +
-        'items.item.design,items.item.description,items.item.specs_route,items.item.has_specs,items.item.alt_names,' +
-        'items.item.name_html,categories,categories.name_html,copyrights,,items.item.has_text,items.item.route,' +
-        'twins.name_html,factories.name_html,moder_votes,moder_voted,votes,of_links,replaceable.name_html';
-
-      return this.changed$.pipe(
-        switchMap(() =>
-          this.pictureService.getPictures$({
-            fields,
-            identity: identity,
-            item_id: current.id,
-            items: {
-              type_id: 1,
-            },
-            limit: 1,
-            paginator: {
-              item_id: current.id,
-            },
+  protected readonly picture$: Observable<Picture> = combineLatest([
+    this.#categoryData$,
+    this.#identity$,
+    this.#changed$,
+  ]).pipe(
+    switchMap(([{current}, identity]) =>
+      this.#picturesClient.getPicture(
+        new PicturesRequest({
+          fields: new PictureFields({
+            copyrights: true,
+            image: true,
+            moderVoted: true,
+            nameHtml: true,
+            nameText: true,
+            paginator: new PicturesRequest({
+              options: new PictureListOptions({
+                pictureItem: new PictureItemListOptions({
+                  itemParentCacheAncestor: new ItemParentCacheListOptions({
+                    parentId: '' + current.id,
+                  }),
+                  typeId: PictureItemType.PICTURE_ITEM_CONTENT,
+                }),
+              }),
+              order: PicturesRequest.Order.ORDER_PERSPECTIVES,
+            }),
+            pictureModerVotes: new PictureModerVoteRequest(),
+            previewLarge: true,
+            replaceable: new PicturesRequest({
+              fields: new PictureFields({nameHtml: true}),
+            }),
+            rights: true,
+            subscribed: true,
+            votes: true,
           }),
-        ),
-        map((response) => (response.pictures.length ? response.pictures[0] : null)),
-      );
-    }),
+          language: this.#languageService.language,
+          options: new PictureListOptions({
+            identity,
+            pictureItem: new PictureItemListOptions({
+              itemParentCacheAncestor: new ItemParentCacheListOptions({
+                parentId: '' + current.id,
+              }),
+              typeId: PictureItemType.PICTURE_ITEM_CONTENT,
+            }),
+          }),
+        }),
+      ),
+    ),
     switchMap((picture) => {
       if (!picture) {
-        this.router.navigate(['/error-404'], {
+        this.#router.navigate(['/error-404'], {
           skipLocationChange: true,
         });
         return EMPTY;
@@ -102,14 +136,14 @@ export class CategoryPictureComponent {
       return of(picture);
     }),
     tap((picture) => {
-      this.pageEnv.set({
+      this.#pageEnv.set({
         pageId: 187,
-        title: picture.name_text,
+        title: picture.nameText,
       });
     }),
   );
 
-  protected readonly currentRouterLinkGallery$ = combineLatest([this.categoryData$, this.identity$]).pipe(
+  protected readonly currentRouterLinkGallery$ = combineLatest([this.#categoryData$, this.#identity$]).pipe(
     map(([{category, current, pathCatnames}, identity]) => {
       if (!category || !identity) {
         return null;
@@ -126,6 +160,6 @@ export class CategoryPictureComponent {
   protected readonly CommentsType = CommentsType;
 
   protected reloadPicture() {
-    this.changed$.next();
+    this.#changed$.next();
   }
 }
