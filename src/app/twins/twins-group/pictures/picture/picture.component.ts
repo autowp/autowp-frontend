c472af2817e269d5c5cabe3e643754836573178c
Dmitry P. <dmitry@pereslegin.ru>
2025-01-25 22:09:34 +0300

    feat: twins/picture via gRPC API

--- src/app/twins/twins-group/pictures/picture/picture.component.ts
+++ src/app/twins/twins-group/pictures/picture/picture.component.ts
@@ -1,42 +1,52 @@
 import {AsyncPipe} from '@angular/common';
 import {Component, inject} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
-import {CommentsType} from '@grpc/spec.pb';
-import {ACLService, Privilege, Resource} from '@services/acl.service';
+import {
+  CommentsType,
+  ItemParentCacheListOptions,
+  Picture,
+  PictureFields,
+  PictureItemListOptions,
+  PictureItemType,
+  PictureListOptions,
+  PictureModerVoteRequest,
+  PicturesRequest,
+} from '@grpc/spec.pb';
+import {PicturesClient} from '@grpc/spec.pbsc';
+import {LanguageService} from '@services/language';
 import {PageEnvService} from '@services/page-env.service';
-import {APIPicture, PictureService} from '@services/picture';
 import {BehaviorSubject, combineLatest, EMPTY, Observable, of} from 'rxjs';
 import {distinctUntilChanged, map, shareReplay, switchMap, tap} from 'rxjs/operators';
 
 import {CommentsComponent} from '../../../../comments/comments/comments.component';
-import {PictureComponent} from '../../../../picture/picture.component';
+import {Picture2Component} from '../../../../picture/picture2.component';
 
 @Component({
-  imports: [PictureComponent, CommentsComponent, AsyncPipe],
+  imports: [CommentsComponent, AsyncPipe, Picture2Component],
   selector: 'app-twins-group-picture',
   templateUrl: './picture.component.html',
 })
 export class TwinsGroupPictureComponent {
-  private readonly route = inject(ActivatedRoute);
-  private readonly pageEnv = inject(PageEnvService);
-  private readonly acl = inject(ACLService);
-  private readonly pictureService = inject(PictureService);
-  private readonly router = inject(Router);
+  readonly #route = inject(ActivatedRoute);
+  readonly #pageEnv = inject(PageEnvService);
+  readonly #router = inject(Router);
+  readonly #picturesClient = inject(PicturesClient);
+  readonly #languageService = inject(LanguageService);
 
-  private readonly changed$ = new BehaviorSubject<void>(void 0);
+  readonly #changed$ = new BehaviorSubject<void>(void 0);
 
-  protected readonly groupId$ = this.route.parent!.parent!.paramMap.pipe(
+  protected readonly groupId$ = this.#route.parent!.parent!.paramMap.pipe(
     map((route) => route.get('group') ?? ''),
     distinctUntilChanged(),
     shareReplay({bufferSize: 1, refCount: false}),
   );
 
-  private readonly identity$ = this.route.paramMap.pipe(
+  readonly #identity$ = this.#route.paramMap.pipe(
     map((route) => route.get('identity')),
     distinctUntilChanged(),
     switchMap((identity) => {
       if (!identity) {
-        this.router.navigate(['/error-404'], {
+        this.#router.navigate(['/error-404'], {
           skipLocationChange: true,
         });
         return EMPTY;
@@ -45,43 +55,56 @@ export class TwinsGroupPictureComponent {
     }),
   );
 
-  protected readonly picture$: Observable<APIPicture> = combineLatest([
+  protected readonly picture$: Observable<Picture> = combineLatest([
     this.groupId$,
-    this.acl.isAllowed$(Resource.SPECIFICATIONS, Privilege.EDIT),
-    this.identity$,
+    this.#identity$,
+    this.#changed$,
   ]).pipe(
-    switchMap(([groupId, isModer, identity]) => {
-      let fields =
-        'owner,name_html,name_text,image,preview_large,paginator,subscribed,taken_date,rights,' +
-        'items.item.design,items.item.description,items.item.specs_route,items.item.has_specs,items.item.alt_names,' +
-        'items.item.name_html,categories.name_html,copyrights,' +
-        'factories.name_html,moder_votes,votes,of_links,replaceable.name_html';
-
-      if (isModer) {
-        fields += ',items.item.brands.name_html';
-      }
-
-      return this.changed$.pipe(
-        switchMap(() =>
-          this.pictureService.getPictures$({
-            fields,
-            identity: identity,
-            item_id: +groupId,
-            items: {
-              type_id: 1,
-            },
-            limit: 1,
-            paginator: {
-              item_id: +groupId,
-            },
+    switchMap(([groupId, identity]) =>
+      this.#picturesClient.getPicture(
+        new PicturesRequest({
+          fields: new PictureFields({
+            copyrights: true,
+            image: true,
+            moderVoted: true,
+            nameHtml: true,
+            nameText: true,
+            paginator: new PicturesRequest({
+              options: new PictureListOptions({
+                pictureItem: new PictureItemListOptions({
+                  itemParentCacheAncestor: new ItemParentCacheListOptions({
+                    parentId: groupId,
+                  }),
+                  typeId: PictureItemType.PICTURE_ITEM_CONTENT,
+                }),
+              }),
+              order: PicturesRequest.Order.ORDER_PERSPECTIVES,
+            }),
+            pictureModerVotes: new PictureModerVoteRequest(),
+            previewLarge: true,
+            replaceable: new PicturesRequest({
+              fields: new PictureFields({nameHtml: true}),
+            }),
+            rights: true,
+            subscribed: true,
+            votes: true,
           }),
-        ),
-        map((response) => (response.pictures.length ? response.pictures[0] : null)),
-      );
-    }),
+          language: this.#languageService.language,
+          options: new PictureListOptions({
+            identity,
+            pictureItem: new PictureItemListOptions({
+              itemParentCacheAncestor: new ItemParentCacheListOptions({
+                parentId: groupId,
+              }),
+              typeId: PictureItemType.PICTURE_ITEM_CONTENT,
+            }),
+          }),
+        }),
+      ),
+    ),
     switchMap((picture) => {
       if (!picture) {
-        this.router.navigate(['/error-404'], {
+        this.#router.navigate(['/error-404'], {
           skipLocationChange: true,
         });
         return EMPTY;
@@ -91,9 +114,9 @@ export class TwinsGroupPictureComponent {
     tap((picture) => {
       setTimeout(
         () =>
-          this.pageEnv.set({
+          this.#pageEnv.set({
             pageId: 28,
-            title: picture.name_text,
+            title: picture.nameText,
           }),
         0,
       );
@@ -103,6 +126,6 @@ export class TwinsGroupPictureComponent {
   protected readonly CommentsType = CommentsType;
 
   protected reloadPicture() {
-    this.changed$.next();
+    this.#changed$.next();
   }
 }
