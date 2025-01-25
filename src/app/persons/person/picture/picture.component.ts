fa911d792d4342d46346d7e1ccf02d1725fb8933
Dmitry P. <dmitry@pereslegin.ru>
2025-01-25 21:39:13 +0300

    feat: persons/picture via gRPC API

--- src/app/persons/person/picture/picture.component.ts
+++ src/app/persons/person/picture/picture.component.ts
@@ -1,34 +1,45 @@
 import {AsyncPipe} from '@angular/common';
 import {Component, inject} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
-import {CommentsType} from '@grpc/spec.pb';
+import {
+  CommentsType,
+  Picture,
+  PictureFields,
+  PictureItemListOptions,
+  PictureItemType,
+  PictureListOptions,
+  PictureModerVoteRequest,
+  PicturesRequest,
+} from '@grpc/spec.pb';
+import {PicturesClient} from '@grpc/spec.pbsc';
+import {LanguageService} from '@services/language';
 import {PageEnvService} from '@services/page-env.service';
-import {APIPicture, PictureService} from '@services/picture';
 import {BehaviorSubject, combineLatest, EMPTY, Observable, of} from 'rxjs';
 import {distinctUntilChanged, map, shareReplay, switchMap, tap} from 'rxjs/operators';
 
 import {CommentsComponent} from '../../../comments/comments/comments.component';
-import {PictureComponent} from '../../../picture/picture.component';
+import {Picture2Component} from '../../../picture/picture2.component';
 
 @Component({
-  imports: [PictureComponent, CommentsComponent, AsyncPipe],
+  imports: [CommentsComponent, AsyncPipe, Picture2Component],
   selector: 'app-persons-person-picture',
   templateUrl: './picture.component.html',
 })
 export class PersonsPersonPictureComponent {
-  private readonly pageEnv = inject(PageEnvService);
-  private readonly route = inject(ActivatedRoute);
-  private readonly pictureService = inject(PictureService);
-  private readonly router = inject(Router);
+  readonly #pageEnv = inject(PageEnvService);
+  readonly #route = inject(ActivatedRoute);
+  readonly #router = inject(Router);
+  readonly #picturesClient = inject(PicturesClient);
+  readonly #languageService = inject(LanguageService);
 
-  private readonly changed$ = new BehaviorSubject<void>(void 0);
+  readonly #changed$ = new BehaviorSubject<void>(void 0);
 
-  protected readonly identity$ = this.route.paramMap.pipe(
+  protected readonly identity$ = this.#route.paramMap.pipe(
     map((route) => route.get('identity')),
     distinctUntilChanged(),
     switchMap((identity) => {
       if (!identity) {
-        this.router.navigate(['/error-404'], {
+        this.#router.navigate(['/error-404'], {
           skipLocationChange: true,
         });
         return EMPTY;
@@ -39,39 +50,59 @@ export class PersonsPersonPictureComponent {
     shareReplay({bufferSize: 1, refCount: false}),
   );
 
-  protected readonly itemID$ = this.route.parent!.paramMap.pipe(
-    map((params) => parseInt(params.get('id') ?? '', 10)),
+  protected readonly itemID$ = this.#route.parent!.paramMap.pipe(
+    map((params) => params.get('id') ?? ''),
     distinctUntilChanged(),
     shareReplay({bufferSize: 1, refCount: false}),
   );
 
-  protected readonly picture$: Observable<APIPicture | null> = combineLatest([this.itemID$, this.identity$]).pipe(
-    switchMap(([itemID, identity]) => {
-      const fields =
-        'owner,name_html,name_text,image,preview_large,paginator,subscribed,taken_date,rights,' +
-        'items.item.design,items.item.description,items.item.specs_route,items.item.has_specs,items.item.alt_names,' +
-        'items.item.name_html,categories.name_html,copyrights,items.item.has_text,items.item.route,' +
-        'twins.name_html,factories.name_html,moder_votes,moder_voted,votes,of_links,replaceable.name_html';
-
-      return this.changed$.pipe(
-        switchMap(() =>
-          this.pictureService.getPictures$({
-            exact_item_id: itemID,
-            fields,
-            identity,
-            limit: 1,
-            paginator: {
-              exact_item_id: itemID,
-            },
+  protected readonly picture$: Observable<null | Picture> = combineLatest([
+    this.itemID$,
+    this.identity$,
+    this.#changed$,
+  ]).pipe(
+    switchMap(([itemID, identity]) =>
+      this.#picturesClient.getPicture(
+        new PicturesRequest({
+          fields: new PictureFields({
+            copyrights: true,
+            image: true,
+            moderVoted: true,
+            nameHtml: true,
+            nameText: true,
+            paginator: new PicturesRequest({
+              options: new PictureListOptions({
+                pictureItem: new PictureItemListOptions({
+                  itemId: itemID,
+                  typeId: PictureItemType.PICTURE_ITEM_AUTHOR,
+                }),
+              }),
+              order: PicturesRequest.Order.ORDER_PERSPECTIVES,
+            }),
+            pictureModerVotes: new PictureModerVoteRequest(),
+            previewLarge: true,
+            replaceable: new PicturesRequest({
+              fields: new PictureFields({nameHtml: true}),
+            }),
+            rights: true,
+            subscribed: true,
+            votes: true,
           }),
-        ),
-        map((response) => (response.pictures.length ? response.pictures[0] : null)),
-      );
-    }),
+          language: this.#languageService.language,
+          options: new PictureListOptions({
+            identity,
+            pictureItem: new PictureItemListOptions({
+              itemId: itemID,
+              typeId: PictureItemType.PICTURE_ITEM_CONTENT,
+            }),
+          }),
+        }),
+      ),
+    ),
     tap((picture) => {
-      this.pageEnv.set({
+      this.#pageEnv.set({
         pageId: 34,
-        title: picture ? picture.name_text : '',
+        title: picture ? picture.nameText : '',
       });
     }),
     shareReplay({bufferSize: 1, refCount: false}),
@@ -80,6 +111,6 @@ export class PersonsPersonPictureComponent {
   protected readonly CommentsType = CommentsType;
 
   protected reloadPicture() {
-    this.changed$.next();
+    this.#changed$.next();
   }
 }
