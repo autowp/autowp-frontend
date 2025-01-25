f183e1fa289a36c20366894379704065196b4ae2
Dmitry P. <dmitry@pereslegin.ru>
2025-01-25 22:32:22 +0300

    feat: picture-page via gRPC API

--- src/app/picture/picture-page.component.ts
+++ src/app/picture/picture-page.component.ts
@@ -2,49 +2,61 @@ import {AsyncPipe} from '@angular/common';
 import {HttpErrorResponse} from '@angular/common/http';
 import {Component, inject} from '@angular/core';
 import {ActivatedRoute, Router, RouterLink} from '@angular/router';
-import {CommentsType} from '@grpc/spec.pb';
+import {
+  CommentsType,
+  Picture,
+  PictureFields,
+  PictureListOptions,
+  PictureModerVoteRequest,
+  PicturesRequest,
+} from '@grpc/spec.pb';
+import {PicturesClient} from '@grpc/spec.pbsc';
 import {APIItem} from '@services/item';
+import {LanguageService} from '@services/language';
 import {PageEnvService} from '@services/page-env.service';
-import {APIPicture, PictureService} from '@services/picture';
+import {PictureService} from '@services/picture';
 import {BehaviorSubject, EMPTY, Observable, of, throwError} from 'rxjs';
 import {catchError, debounceTime, distinctUntilChanged, map, switchMap, tap} from 'rxjs/operators';
 
 import {PathItem} from '../categories/service';
 import {CommentsComponent} from '../comments/comments/comments.component';
-import {PictureComponent} from './picture.component';
+import {Picture2Component} from './picture2.component';
 
 @Component({
-  imports: [RouterLink, PictureComponent, CommentsComponent, AsyncPipe],
+  imports: [RouterLink, CommentsComponent, AsyncPipe, Picture2Component],
   selector: 'app-picture-page',
   templateUrl: './picture-page.component.html',
 })
 export class PicturePageComponent {
-  private readonly route = inject(ActivatedRoute);
-  private readonly pageEnv = inject(PageEnvService);
-  private readonly pictureService = inject(PictureService);
-  private readonly router = inject(Router);
+  readonly #route = inject(ActivatedRoute);
+  readonly #pageEnv = inject(PageEnvService);
+  readonly #pictureService = inject(PictureService);
+  readonly #router = inject(Router);
+  readonly #picturesClient = inject(PicturesClient);
+  readonly #languageService = inject(LanguageService);
+
+  readonly #changed$ = new BehaviorSubject<void>(void 0);
 
   protected category: APIItem | null = null;
   protected current: APIItem | null = null;
   protected path: PathItem[] = [];
-  private readonly changed$ = new BehaviorSubject<void>(void 0);
 
-  protected readonly picture$: Observable<APIPicture | null> = this.route.paramMap.pipe(
+  protected readonly picture$: Observable<null | Picture> = this.#route.paramMap.pipe(
     map((route) => route.get('identity')),
     distinctUntilChanged(),
     debounceTime(10),
     switchMap((identity) => {
       if (!identity) {
-        this.router.navigate(['/error-404'], {
+        this.#router.navigate(['/error-404'], {
           skipLocationChange: true,
         });
         return EMPTY;
       }
 
-      return this.pictureService.getCanonicalRoute$(identity).pipe(
+      return this.#pictureService.getCanonicalRoute$(identity).pipe(
         catchError((response: unknown) => {
           if (response instanceof HttpErrorResponse && response.status === 404) {
-            this.router.navigate(['/error-404'], {
+            this.#router.navigate(['/error-404'], {
               skipLocationChange: true,
             });
             return EMPTY;
@@ -53,43 +65,52 @@ export class PicturePageComponent {
         }),
         switchMap((route) => {
           if (route && route.length > 0) {
-            this.router.navigate(route, {
+            this.#router.navigate(route, {
               replaceUrl: true,
             });
             return EMPTY;
           }
 
-          const fields =
-            'owner,name_html,name_text,image,preview_large,paginator,subscribed,taken_date,rights,' +
-            'items.item.design,items.item.description,items.item.specs_route,items.item.has_specs,items.item.alt_names,' +
-            'items.item.name_html,categories.name_html,copyrights,' +
-            'twins.name_html,factories.name_html,moder_votes,moder_voted,votes,of_links,replaceable.name_html';
-
-          return this.changed$.pipe(
-            switchMap(() =>
-              this.pictureService.getPictures$({
-                fields,
-                identity,
-                limit: 1,
+          return this.#changed$;
+        }),
+        switchMap(() =>
+          this.#picturesClient.getPicture(
+            new PicturesRequest({
+              fields: new PictureFields({
+                copyrights: true,
+                image: true,
+                moderVoted: true,
+                nameHtml: true,
+                nameText: true,
+                pictureModerVotes: new PictureModerVoteRequest(),
+                previewLarge: true,
+                replaceable: new PicturesRequest({
+                  fields: new PictureFields({nameHtml: true}),
+                }),
+                rights: true,
+                subscribed: true,
+                votes: true,
               }),
-            ),
-            switchMap((response) => {
-              if (response.pictures.length <= 0) {
-                this.router.navigate(['/error-404'], {
-                  skipLocationChange: true,
-                });
-                return EMPTY;
-              }
-              return of(response.pictures[0]);
+              language: this.#languageService.language,
+              options: new PictureListOptions({identity}),
             }),
-          );
+          ),
+        ),
+        switchMap((picture) => {
+          if (!picture) {
+            this.#router.navigate(['/error-404'], {
+              skipLocationChange: true,
+            });
+            return EMPTY;
+          }
+          return of(picture);
         }),
       );
     }),
     tap((picture) => {
-      this.pageEnv.set({
+      this.#pageEnv.set({
         pageId: 187,
-        title: picture ? picture.name_text : '',
+        title: picture ? picture.nameText : '',
       });
     }),
   );
@@ -97,6 +118,6 @@ export class PicturePageComponent {
   protected readonly CommentsType = CommentsType;
 
   protected reloadPicture() {
-    this.changed$.next();
+    this.#changed$.next();
   }
 }
