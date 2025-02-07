446894c5b2439d5d9322125614e71a7c0f08e8d2
Dmitry P. <dmitry@pereslegin.ru>
2025-02-08 02:55:58 +0300

    feat: cutaway authors via gRPC API

--- src/app/cutaway/authors/authors.component.ts
+++ src/app/cutaway/authors/authors.component.ts
@@ -1,14 +1,28 @@
 import {AsyncPipe} from '@angular/common';
 import {Component, inject, OnInit} from '@angular/core';
 import {ActivatedRoute, RouterLink} from '@angular/router';
-import {ItemType} from '@grpc/spec.pb';
-import {APIItem, ItemService} from '@services/item';
+import {
+  APIItem,
+  ItemFields,
+  ItemListOptions,
+  ItemParentCacheListOptions,
+  ItemsRequest,
+  ItemType,
+  PictureFields,
+  PictureItemListOptions,
+  PictureItemType,
+  PictureListOptions,
+  PictureStatus,
+  PreviewPicturesRequest,
+} from '@grpc/spec.pb';
+import {ItemsClient} from '@grpc/spec.pbsc';
+import {LanguageService} from '@services/language';
 import {PageEnvService} from '@services/page-env.service';
 import {
-  CatalogueListItem,
-  CatalogueListItemComponent,
-  CatalogueListItemPicture,
-} from '@utils/list-item/list-item.component';
+  CatalogueListItem2,
+  CatalogueListItem2Component,
+  CatalogueListItemPicture2,
+} from '@utils/list-item/list-item2.component';
 import {EMPTY} from 'rxjs';
 import {catchError, debounceTime, distinctUntilChanged, map, switchMap} from 'rxjs/operators';
 
@@ -16,86 +30,102 @@ import {PaginatorComponent} from '../../paginator/paginator/paginator.component'
 import {ToastsService} from '../../toasts/toasts.service';
 
 @Component({
-  imports: [RouterLink, CatalogueListItemComponent, PaginatorComponent, AsyncPipe],
+  imports: [RouterLink, PaginatorComponent, AsyncPipe, CatalogueListItem2Component],
   selector: 'app-cutaway-authors',
   templateUrl: './authors.component.html',
 })
 export class CutawayAuthorsComponent implements OnInit {
-  private readonly itemService = inject(ItemService);
-  private readonly route = inject(ActivatedRoute);
-  private readonly pageEnv = inject(PageEnvService);
-  private readonly toastService = inject(ToastsService);
+  readonly #route = inject(ActivatedRoute);
+  readonly #pageEnv = inject(PageEnvService);
+  readonly #toastService = inject(ToastsService);
+  readonly #languageService = inject(LanguageService);
+  readonly #itemsClient = inject(ItemsClient);
 
-  protected readonly query$ = this.route.queryParamMap.pipe(
+  protected readonly query$ = this.#route.queryParamMap.pipe(
     map((params) => parseInt(params.get('page') ?? '', 10)),
     distinctUntilChanged(),
     debounceTime(30),
     switchMap((page) =>
-      this.itemService.getItems$({
-        descendant_pictures: {
-          contains_perspective_id: 9,
-          status: 'accepted',
-          type_id: 2,
-        },
-        fields:
-          'name_html,name_default,description,has_text,preview_pictures.route,preview_pictures.picture.name_text,total_pictures',
-        limit: 12,
-        page,
-        preview_pictures: {
-          contains_perspective_id: 9,
-          type_id: 2,
-        },
-        type_id: ItemType.ITEM_TYPE_PERSON,
-      }),
+      this.#itemsClient.list(
+        new ItemsRequest({
+          fields: new ItemFields({
+            description: true,
+            hasText: true,
+            nameDefault: true,
+            nameHtml: true,
+            previewPictures: new PreviewPicturesRequest({
+              containsPerspectiveId: 9,
+              picture: new PictureFields({nameText: true}),
+              typeId: PictureItemType.PICTURE_ITEM_AUTHOR,
+            }),
+          }),
+          language: this.#languageService.language,
+          limit: 12,
+          options: new ItemListOptions({
+            descendant: new ItemParentCacheListOptions({
+              pictureItemsByItemId: new PictureItemListOptions({
+                pictures: new PictureListOptions({
+                  pictureItem: new PictureItemListOptions({perspectiveId: 9}),
+                  status: PictureStatus.PICTURE_STATUS_ACCEPTED,
+                }),
+                typeId: PictureItemType.PICTURE_ITEM_AUTHOR,
+              }),
+            }),
+            typeId: ItemType.ITEM_TYPE_PERSON,
+          }),
+          order: ItemsRequest.Order.AGE,
+          page,
+        }),
+      ),
     ),
     catchError((response: unknown) => {
-      this.toastService.handleError(response);
+      this.#toastService.handleError(response);
       return EMPTY;
     }),
     map((response) => ({
-      items: this.prepareItems(response.items),
+      items: this.prepareItems(response.items || []),
       paginator: response.paginator,
     })),
   );
 
   ngOnInit(): void {
-    setTimeout(() => this.pageEnv.set({pageId: 201}), 0);
+    setTimeout(() => this.#pageEnv.set({pageId: 201}), 0);
   }
 
-  private prepareItems(items: APIItem[]): CatalogueListItem[] {
+  private prepareItems(items: APIItem[]): CatalogueListItem2[] {
     return items.map((item) => {
       const itemRouterLink = ['/persons'];
-      itemRouterLink.push(item.id.toString());
+      itemRouterLink.push(item.id);
 
-      const pictures: CatalogueListItemPicture[] = item.preview_pictures.pictures.map((picture) => ({
-        picture: picture?.picture ? picture.picture : null,
-        routerLink: picture?.picture ? itemRouterLink.concat([picture.picture.identity]) : [],
-        thumb: picture ? picture.thumb : null,
+      const pictures: CatalogueListItemPicture2[] = (item.previewPictures?.pictures || []).map((picture) => ({
+        picture: picture ? picture : null,
+        routerLink: picture ? itemRouterLink.concat([picture.identity]) : [],
+        thumb: picture ? picture.thumbMedium : null,
       }));
 
       return {
-        accepted_pictures_count: item.accepted_pictures_count,
-        can_edit_specs: false,
-        childs_counts: null,
+        acceptedPicturesCount: undefined,
+        canEditSpecs: false,
+        childsCounts: null,
         description: item.description,
         design: null,
         details: {
-          count: item.childs_count,
+          count: 0,
           routerLink: itemRouterLink,
         },
         engine_vehicles: undefined,
-        has_text: !!item.has_text,
+        hasText: item.hasText,
         id: item.id,
-        item_type_id: item.item_type_id,
-        name_default: item.name_default,
-        name_html: item.name_html,
+        itemTypeId: item.itemTypeId,
+        nameDefault: item.nameDefault,
+        nameHtml: item.nameHtml,
         picturesRouterLink: itemRouterLink,
-        preview_pictures: {
-          large_format: item.preview_pictures.large_format,
+        previewPictures: {
+          largeFormat: !!item.previewPictures?.largeFormat,
           pictures,
         },
         produced: null,
-        produced_exactly: null,
+        producedExactly: null,
         specsRouterLink: null,
       };
     });
